7a19503f79771d7f84e979b6af0b8b0ae652bf74
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-04-19 16:07:59 +0430

    Added event listeners to Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { createContext, useState } from "react";
 import Header from "./Header";
 
+export const InputContext = createContext();
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
+
+  const value = {
+    inputValue,
+    setInputValue,
+  };
+
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <InputContext.Provider value={value}>
+      <div className="App">
+        <Header />
+      </div>
+    </InputContext.Provider>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { InputContext } from "./App";
 
 function Header() {
+  const [value, setValue] = useState("");
+  const { inputValue, setInputValue } = useContext(InputContext);
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    setValue(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setInputValue(value);
+    setValue("");
+  };
+
   return (
     <div className="Header">
       <div className="container mx-auto py-8 text-center">
         <h1 className="text-3xl font-bold">Dictionary</h1>
         <h3 className="mt-1">Find the definition of a word</h3>
         <div className="flex items-center justify-center mt-5">
-          <div className="flex border-2 rounded">
+          <form className="flex border-2 rounded">
             <input
-              type="text"
+              type="search"
               placeholder="Type a word"
               className="px-4 py-2 md:w-80"
+              onChange={handleInput}
+              value={value}
             />
-            <button className="bg-blue-400 rounded border-l px-4 py-2">
+            <button
+              type="submit"
+              className="bg-blue-400 rounded border-l px-4 py-2"
+              onClick={handleClick}
+            >
               Search
             </button>
-          </div>
+          </form>
         </div>
-        <h2>Word</h2>
+        {inputValue && (
+          <h2 className="mt-10 font-bold text-xl">{inputValue}</h2>
+        )}
       </div>
     </div>
   );
