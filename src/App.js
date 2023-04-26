1445b5f3856bf38ffaf9da62120b362ac300b3ec
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-04-26 20:12:15 +0430

    Created Results component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
 import Header from "./Header";
+import Results from "./Results";
 
 export const InputContext = createContext();
 
@@ -15,6 +16,7 @@ function App() {
     <InputContext.Provider value={value}>
       <div className="App">
         <Header />
+        <Results />
       </div>
     </InputContext.Provider>
   );
---
+++ src/Results.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Results() {
+  return (
+    <div className="Results">
+      <div className="container mx-auto p-4 max-w-2xl">
+        <h3 className=" text-2xl font-bold mt-4">Definisions:</h3>
+        <h3 className=" text-2xl font-bold mt-4">Examples:</h3>
+        <h3 className=" text-2xl font-bold mt-4">Synonyms:</h3>
+        <h3 className=" text-2xl font-bold mt-4">Antonyms:</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Results;
