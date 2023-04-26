6a0f00609f09463b939cbd212aad80643690e4f1
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-04-26 20:49:07 +0430

    Created Definitions component

---
+++ src/Definitions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Definitions() {
+  return <div className="Definitions">Definitions</div>;
+}
+
+export default Definitions;
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { InputContext } from "./App";
+import Definitions from "./Definitions";
 
 axios.defaults.baseURL = `https://api.dictionaryapi.dev/api/v2/entries/en`;
 
@@ -42,7 +43,8 @@ function Results() {
       <div className="container mx-auto p-4 max-w-2xl">
         {response && (
           <div>
-            <h3 className="font-bold mt-4">Definisions:</h3>
+            <h3 className="font-bold mt-4">Definitions:</h3>
+            <Definitions />
             <h3 className="font-bold mt-4">Examples:</h3>
             <h3 className="font-bold mt-4">Synonyms:</h3>
             <h3 className="font-bold mt-4">Antonyms:</h3>
