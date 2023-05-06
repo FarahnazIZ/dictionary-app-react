ae3d652202b4e312f22829b84440eeddc682dc34
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-05-07 00:27:55 +0430

    Added footer

--- src/App.js
+++ src/App.js
@@ -17,6 +17,25 @@ function App() {
       <div className="App font-mono">
         <Header />
         <Results />
+        <footer className="text-center my-5">
+          <a
+            href="https://github.com/FarahnazIZ/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+            className=" text-lime-500 hover:underline"
+          >
+            Open-source code
+          </a>
+          , by{" "}
+          <a
+            href="https://curious-flan-f035bf.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className=" text-lime-500 hover:underline"
+          >
+            Farahnaz Izadi
+          </a>
+        </footer>
       </div>
     </InputContext.Provider>
   );
